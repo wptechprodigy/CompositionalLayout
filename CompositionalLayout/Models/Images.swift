3a1dd350a33414c8c6cfe2a3369249745e35e909
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-09 13:16:15 +0100

    modify old time flow layout to composite layout

---
+++ CompositionalLayout/Models/Images.swift
@@ -0,0 +1,12 @@
+//
+//  Images.swift
+//  CompositionalLayout
+//
+//  Created by waheedCodes on 09/05/2021.
+//
+
+import Foundation
+
+struct Images {
+    
+}
--- CompositionalLayout/View Controllers/Root View Controller/RootViewController.swift
+++ CompositionalLayout/View Controllers/Root View Controller/RootViewController.swift
@@ -13,7 +13,7 @@ class RootViewController: UIViewController {
     
     private let collectionView: UICollectionView = UICollectionView(
         frame: .zero,
-        collectionViewLayout: UICollectionViewFlowLayout()
+        collectionViewLayout: RootViewController.composeLayout()
     )
     
     // MARK: - Life Cycle Methods
@@ -33,6 +33,34 @@ class RootViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         collectionView.frame = view.bounds
+    }
+    
+    // MARK: - Helper Methods
+    
+    static func composeLayout() -> UICollectionViewCompositionalLayout {
+        
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+
+        // Group
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(2/5)
+            ),
+            subitem: item,
+            count: 2
+        )
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
 }
 
