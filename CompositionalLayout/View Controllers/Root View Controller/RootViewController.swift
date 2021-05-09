80e24f294324619931e69d51f09d716997ccbe23
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-09 09:45:46 +0100

    Initial project setup

---
+++ CompositionalLayout/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  CompositionalLayout
+//
+//  Created by waheedCodes on 09/05/2021.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    // MARK: - Life Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
