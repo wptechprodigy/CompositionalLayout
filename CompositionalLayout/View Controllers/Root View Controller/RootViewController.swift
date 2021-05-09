18c530427b98e6645a6b1b7e9e1ac83571ecb695
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-09 12:46:39 +0100

    create a grid using old time flow layout

--- CompositionalLayout/View Controllers/Root View Controller/RootViewController.swift
+++ CompositionalLayout/View Controllers/Root View Controller/RootViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class RootViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
     // MARK: - Life Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.register(
+            CollectionViewCell.self,
+            forCellWithReuseIdentifier: CollectionViewCell.identifier
+        )
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView.frame = view.bounds
+    }
+}
 
+extension RootViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier,
+                                                            for: indexPath) as? CollectionViewCell else {
+            fatalError("Cell could not be created!")
+        }
+        
+        return cell
+    }
 }
